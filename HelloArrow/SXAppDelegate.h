7ad99f90b177e2c5799ff93842c0f49c2a380573
ShinSuo <shinsuo1988@gmail.com>
2012-10-28 13:00:32 +0000

    Initial Commit

---
+++ HelloArrow/SXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SXAppDelegate.h
+//  HelloArrow
+//
+//  Created by shin on 12-10-28.
+//  Copyright (c) 2012年 Indie Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloArrow/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloArrow
+//
+//  Created by shin on 12-10-28.
+//  Copyright (c) 2012年 Indie Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SXAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SXAppDelegate class]));
+    }
+}
