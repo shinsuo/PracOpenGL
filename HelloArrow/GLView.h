3d837426058bbaa6bb9d92043d67583ec163dc33
shin <suoxin117@gmail.com>
2012-11-01 14:08:24 +0800

    分离native分支，便于练习

--- HelloArrow/GLView.h
+++ HelloArrow/GLView.h
@@ -13,6 +13,38 @@
 #import <OpenGLES/ES1/gl.h>
 #import <OpenGLES/ES1/glext.h>
 
+typedef struct {
+    GLfloat x;
+    GLfloat y;
+    GLfloat z;
+}Vertex3D;
+
+typedef struct {
+    Vertex3D v1;
+    Vertex3D v2;
+    Vertex3D v3;
+}Triangle3D;
+
+static inline Vertex3D Vertex3DMake(GLfloat x,GLfloat y,GLfloat z)
+{
+    Vertex3D v3D;
+    v3D.x = x; v3D.y = y; v3D.z = z;
+    return v3D;
+}
+
+static inline Triangle3D Triangle3DMake(Vertex3D v1,Vertex3D v2,Vertex3D v3)
+{
+    Triangle3D t3D;
+    t3D.v1 = v1; t3D.v2 = v2; t3D.v3 = v3;
+    return t3D;
+}
+
+static inline void Vertex3DSet(Vertex3D *vertex,CGFloat inX,CGFloat inY,CGFloat inZ)
+{
+    vertex->x = inX;    vertex->y = inY;    vertex->z = inZ;
+}
+
+
 @interface GLView : UIView
 {
     EAGLContext *m_context;
--- HelloArrow/SXAppDelegate.mm
+++ HelloArrow/SXAppDelegate.mm
@@ -22,7 +22,9 @@
     CGRect screenBounds = [[UIScreen mainScreen] bounds];
     
     self.window = [[[UIWindow alloc] initWithFrame:screenBounds] autorelease];
-    m_view = [[GLView alloc] initWithFrame:screenBounds];
+    m_view = [[GLView alloc] initWithFrame:CGRectMake(10, 10, 300, 460)];
+    
+    [application setStatusBarHidden:YES];
     
     [self.window addSubview:m_view];
     // Override point for customization after application launch.
